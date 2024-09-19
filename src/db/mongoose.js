2f9a0d6bff71b9ac8a2e654965135e24ca288c38
maanassomani <115005588+maanassomani@users.noreply.github.com>
2024-09-19 17:38:07 +0530

    Mongodb runs on localhost

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 async function main() {
     try {
-        await mongoose.connect(process.env.MONGODB_URL)
+        await mongoose.connect(process.env.MONGODB_URL || "mongodb://localhost:27017/")
         console.log('database connection successful!')
     }
     catch (error) {
